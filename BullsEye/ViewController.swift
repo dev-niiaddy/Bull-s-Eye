c156b9474bb64e37ef193e3df1c4e6c05e0720e3
Addy Godwin Nii <dev.niiaddy@gmail.com>
2020-06-02 21:06:44 +0000

    added app icons

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -92,6 +92,13 @@ class ViewController: UIViewController {
     }
     
     func startNewRound(){
+        let transition = CATransition()
+        transition.type = CATransitionType.fade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        view.layer.add(transition, forKey: nil)
+        
+        
         round += 1
         targetValue = Int.random(in: 1...100)
         currentSliderValue = 50
